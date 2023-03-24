7caaed4895bd64edc71312957cf1793730c1f244
Nabil GAYL <gaylnabil@gmail.com>
2023-03-24 06:47:08 +0000

    updating style for Todo List

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -14,9 +14,18 @@ const TodoList: FC<IProps> = ({ todoList, setTodoList }) => {
   const todoElements = todoList.map((todo) => {
     return <SingleTodo key={todo.id} todo={todo} setTodoList={setTodoList} />;
   });
+
   return (
-    <div className="content__todos">
-      <div className="content__todos__list">{todoElements}</div>
+    <div className="container__todos">
+      {/* <div className="container__todos__list">{todoElements}</div> */}
+      <div className="container__todos__list">
+        <h2 className="container__todos__heading">Active Tasks</h2>
+        {todoElements}
+      </div>
+      <div className="container__todos__list completed">
+        <h2 className="container__todos__heading">Task Completed</h2>
+        {todoElements}
+      </div>
     </div>
   );
 };
